7fd78f4234d42b4c5748f79957b1c187e54fa328
Angel Adrian De la Cruz Castillo <61015421+caztle1304@users.noreply.github.com>
2023-02-24 16:29:15 -0800

    Update removeMASTcluster_myeloid_Feb15_2023.R

--- R/removeMASTcluster_myeloid_Feb15_2023.R
+++ R/removeMASTcluster_myeloid_Feb15_2023.R
@@ -1,3 +1,13 @@
+############    -  Removing MAST cells cluster -    ############
+
+# ---
+# Author: Angel Adrian De la Cruz Castillo
+# Date: 2023-02-15
+# ---
+
+### -------------------------- Description -------------------------- ###
+# This script will remove cluster of MAST cells from myeloid cells and create a new ArchR project to avoid overwriting
+
 library(ArchR)
 
 myeloid <- loadArchRProject('/mnt/bioadhoc-temp/Groups/vd-vijay/acastillo/R24/singleLane_4paper/projects/myeloid_R24DICE_Hu_M2_mye_ATAC_TSS8_nFragslog3.5_varFeats30k')
