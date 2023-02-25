9fdcdc2a46692c1bdadf89ef65a494d3565590dd
Angel Adrian De la Cruz Castillo <61015421+caztle1304@users.noreply.github.com>
2023-02-24 16:38:37 -0800

    Update unconstrainedIntegration.R

--- R/unconstrainedIntegration.R
+++ R/unconstrainedIntegration.R
@@ -65,53 +65,6 @@ removeSeuratGenes <- function(so, archrproj)
 
 
 
-
-plotPredictionScorePerCelltype <- function(archrproj, suffix)
-{
-  ## Plots violin plot with boxplot for scoring of each cell type
-cellColData <- getCellColData(archrproj, select = paste0(c('predictedGroup_', 'predictedScore_'), suffix))
-cellColData <- as.data.frame(cellColData)
-
-p <- ggplot(cellColData, aes(x = get(paste0('predictedGroup_', suffix)), y = get(paste0('predictedScore_', suffix)), fill = get(paste0('predictedGroup_', suffix)))) +
-     geom_violin(width = 0.5, alpha = 0.2) +
-     geom_boxplot(width = 0.1) +
-     theme_bw() +
-     ggtitle('Prediction scores by group') +
-     theme(legend.position = 'none', axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)) +
-     xlab('Cell type') + ylab('Prediction score')
-return(p)
-}
-
-plotCellsPerCellType <- function(archrproj, suffix)
-{ ## Plots number of cells of each identified cell type
-  cellColData <- getCellColData(archrproj, select = paste0('predictedGroup_', suffix))
-  cellColData <- as.data.frame(cellColData)
-  df <- as.data.frame(table(cellColData))
-  plot <- ggplot(df, aes(x = cellColData, y = Freq, fill = cellColData)) + geom_bar(stat = 'identity') +
-          geom_text(aes(label = Freq), vjust = 1.6, color = 'black', size = 3.5) + ggtitle('Number of cells per cell type') +
-          theme(legend.position = 'none', axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)) + xlab('Cell Type') + ylab('nCells')
-  return(plot)
-
-}
-
-plotCellTypeProportions <- function(archrproj, suffix)
-{ ## Plots pie chart showing proportions of each identified cell type
-  cellColData <- getCellColData(archrproj, select = paste0('predictedGroup_', suffix))
-  df <- as.data.frame(table(as.data.frame(cellColData)))
-  df <- df[order(df$Freq),]
-  df$Var1 <- factor(df$Var1, levels = df$Var1)
-  df <- df %>% mutate(Perc = paste0(round(Freq/sum(Freq)*100, digits = 2), '%'), text_y = sum(Freq) - (cumsum(Freq) - Freq/2)) ## text_y is for position of each label in pie chart
-  plot <- ggplot(df, aes(x="", y=Freq, fill=Var1)) +
-  geom_bar(stat="identity", width=1, color="white") +
-  coord_polar("y", start=0) +
-  theme_void() + # remove background, grid, numeric labels
-  ggtitle('Cell Type proportions') +
-  geom_label_repel(aes(label = Perc, y = text_y), nudge_x = .6, nudge_y = .6, show.legend = F, size = 5) +
-  guides(fill = guide_legend(title = 'Cell type'))
-  return(plot)
-}
-
-
 ## main()
 
 so_rna <- readRDS(args$SeuratObject)
