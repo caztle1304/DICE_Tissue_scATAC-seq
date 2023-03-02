7161c5d4a2e041a40ada14e14fe456f16408f48c
Angel Adrian De la Cruz Castillo <61015421+caztle1304@users.noreply.github.com>
2023-03-02 14:17:15 -0800

    Create run_createProjects.sh

---
+++ sh/run_createProjects.sh
@@ -0,0 +1,7 @@
+## To create initial projects
+
+/mnt/BioApps/R/4.0.1/bin/Rscript createProjects.R
+
+## To create projects without using any filter, for QC purposes
+
+/mnt/BioApps/R/4.0.1/bin/Rscript createUnfilteredProjects.R
